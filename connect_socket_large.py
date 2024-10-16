d56b46c6238492a4d8b2ced2b1a000b75acf2808
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-16 17:55:07 -0600

    Improve code for large socket response.

---
+++ connect_socket_large.py
@@ -0,0 +1,42 @@
+import socket
+
+# define server uri, port of recieving socket & route to file
+host = 'gaia.cs.umass.edu'
+port = 80
+file = '/wireshark-labs/HTTP-wireshark-file3.html'
+method = 'GET'
+
+# create the socket
+client_socket = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM)
+
+# connect to host at web server port
+client_socket.connect(
+    (host, port))
+
+# build HTTP GET request
+request = f"{method} {file} HTTP/1.1\r\nHost:{host}\r\n\r\n"
+
+# send request
+client_socket.sendall(
+    request.encode())
+
+# accept a response from the server (as a byte literal)
+response = b""
+
+# loop as long as client is recieving data from server
+while True:
+    # read a chunk of bytes from server through socket
+    chunk = client_socket.recv(4096)
+    if not chunk: # no data to read
+        break
+    # assemble payload
+    response += chunk
+
+# close the socket
+client_socket.close()
+
+# print full response containing headers and content
+print(response.decode(
+    'utf-8'))
